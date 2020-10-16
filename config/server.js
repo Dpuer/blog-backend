a92d2e4d1a49db93505055d00325bddbb39c8305
羽扬 <392165133@qq.com>
2020-10-17 02:19:32 +0800

    初始化项目

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e7ba3256273ccef2434daf16f27f8a9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eefbde67-3b9d-478c-bcc8-ab73e0f51864'
+};
